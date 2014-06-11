b74db86e263159566aec5f27290858c1e22c870d
shioshin <shioshin@localhost.localdomain>
2014-06-11 23:29:39 +0300

    added broadcaster

--- src/main/java/com/test/cluster/MyVaadinUI.java
+++ src/main/java/com/test/cluster/MyVaadinUI.java
@@ -1,42 +1,60 @@
 package com.test.cluster;
 
-import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import javax.servlet.annotation.WebServlet;
 
 @Theme("mytheme")
 @SuppressWarnings("serial")
-@Push
-public class MyVaadinUI extends UI
-{
+public class MyVaadinUI extends UI implements Broadcaster.BroadcastListener {
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.test.cluster.AppWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+    VerticalLayout layout = new VerticalLayout();
+
 
     @Override
     protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
+        Broadcaster.register(this);
         layout.setMargin(true);
         setContent(layout);
-        
+
+        Label getSession = new Label("Session : " + getSession().getSession().getId());
+        final String sessionString = "Id : " + getId() + 
+                " sessionId : " + getSession().getSession().getId() + 
+                " isNew : " + getSession().getSession().isNew();
+        layout.addComponent(getSession);
+
         Button button = new Button("Click Me");
         button.addClickListener(new Button.ClickListener() {
             @Override
             public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+                Broadcaster.broadcast(sessionString);
             }
         });
+        button.setDebugId("debugId");
         layout.addComponent(button);
+    }
+
+    // Must also unregister when the UI expires    
+    @Override
+    public void detach() {
+        Broadcaster.unregister(this);
+        super.detach();
+    }
+
+    @Override
+    public void receiveBroadcast(final String message) {
+        // Must lock the session to execute logic safely
+        access(new Runnable() {
+            @Override
+            public void run() {
+                // Show it somehow
+                layout.addComponent(new Label(message));
+            }
+        });
     }
 
 }
